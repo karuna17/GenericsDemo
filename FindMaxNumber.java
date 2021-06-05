15b7771621d82baf0f1fdd84920d140b76483927
karuna17 <karunakakade8@gmail.com>
2021-06-06 00:41:35 +0530

    Added: method with three parameters and apply sorting method

--- FindMaxNumber.java
+++ FindMaxNumber.java
@@ -10,7 +10,46 @@ public class FindMaxNumber {
 			}
 		}
 		System.out.println("Maximum Number: "+ max);
+	}
 
+	public static  <T extends  Comparable <T>> void maxFind(Integer[] a, Float[] b, String[] c) {
+		System.out.println("Sorting Of Integer Array");
+		int temp = 0;
+		for(int i = 0; i < a.length; i++) {
+			for(int j = i + 1; j < a.length; j++) {
+				if(a[i]>a[j]) {
+					temp = a[i];
+					a[i] = a[j];
+					a[j] = temp;
+				}
+			}
+		}
+		printArr(a);
+
+		System.out.println("Sorting Of Float Array");
+		float temp1 = 0.0f;
+		for(float i = 0.0f; i < b.length; i++) {
+			for(float j = i + 1.0f; j < b.length; j++) {
+				if(b[(int) i] > b[(int) j]) {
+					temp1 = b[(int) i];
+					b[(int) i] = b[(int) j];
+					b[(int) j] = temp1;
+				}
+			}
+		}
+		printArr(b);
+
+		System.out.println("Sorting Of String Array");
+		for(int i = 0; i<c.length; i++) {  
+			for (int j = i+1; j<c.length; j++) {  
+				if(c[i].compareTo(c[j])>0) {  
+					String temp2 = c[i];  
+					c[i] = c[j];  
+					c[j] = temp2;  
+				}  
+			}  
+		}
+		printArr(c);
 	}
 
 	public static <T extends Comparable<T>> void findmaxString(T[] elements) {
@@ -25,27 +64,28 @@ public class FindMaxNumber {
 
 	public static<E>void printArr(E[] element) {	 
 		for(E i : element ) {
-			System.out.print(i+" ");
+			System.out.print(i+"  ");
 		}
 		System.out.println();      
 	}
 
-
 	public static void main(String[] args) {
-		Integer[] integerArray= {10,20,30,40,10};
-		
-		Double[] doubleArr= {100.0,200.0,300.0,400.0};
-		
-		String[] stringArr= {"strawberry","pineapple","guvava","tim","do"};
+		Integer[] integerArray= {110,20,35,100,10};
+
+		Float[] floatArr= {400.0f,600.0f,100.0f,500.0f};
+
+		String[] stringArr= {"strawberry","pineapple","guvava","apple","orange"};
 
 		printArr(integerArray);
 		maxFind(integerArray);
 
-		printArr(doubleArr);
-		maxFind(doubleArr);
+		printArr(floatArr);
+		maxFind(floatArr);
 
 		printArr(stringArr);
 		maxFind(stringArr);
+
+		maxFind(integerArray, floatArr, stringArr);
 
 	}
 }
